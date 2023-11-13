88dd5f9657b24aa889e3689bd709c02490f5c616
elify <elifyagmurduran@gmail.com>
2023-11-13 14:28:41 +0100

    start step 2

---
+++ step2_card-scheme/step2.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const card_scheme = express();
+const port_num = 3001;
+card_scheme.get("", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+app.listen(port, () => {
+  console.log(`server is running - port: ${port_num}`);
+});
